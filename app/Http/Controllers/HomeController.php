f8793e2c17ef02db4db951cd1c7aa980b19d3a8f
zxczxczxc123123123 <nadein111@gmail.com>
2020-06-10 10:05:00 +0700

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Product;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index(){
+        $products = Product::orderby('created_at')->take(8)->get();
+
+        return view('home.index', [
+            'products' => $products
+        ]);
+    }
+}
